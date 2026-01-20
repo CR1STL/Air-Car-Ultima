71ffb099d8c9836f638b48282aabf37d03e463f0
CR1STL <96071930+CR1STL@users.noreply.github.com>
2026-01-20 22:01:51 +0300

    Update Car_Wall_Scr.cs

--- Air Car Ultima/Assets/Scripts/Car_Wall_Scr.cs
+++ Air Car Ultima/Assets/Scripts/Car_Wall_Scr.cs
@@ -24,6 +24,8 @@ public class Car_Wall_Scr : MonoBehaviour
         }
         if (other.tag == "Enemy")
         {
+vpn://AAAA_3icdY1LC4JAFEb_isy6wqLQ2kUJQghRUNRGBp10tHkwD83E_97MSLRqdeGew3d6QCFBYOOBLaHojaF3FIhgTcDEAzmSmcBcYUb_GBmjD1ykDRJylBbmCTlOR2AePZBINDhDqeq4C8FxZsp_M1-FC6ZYxp5OawuLtGFmTVMlOnNzNyE0GGxIqzLNoYKuY7M16iy_zpPKn5frWbhvV6fL4RUnbRvqII_EWd7KOg6iZaXv_g4MwwcyO1
+        
             Destroy(other.gameObject);
         }
     }
